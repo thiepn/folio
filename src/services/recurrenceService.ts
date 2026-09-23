e3f3e524229f838fe077ca654c1e5417c7ef9b5d
thiepn <junsosmart@gmail.com>
2026-09-23 17:08:42 +0200

    fix(organization): synchronize one-off tag ids with edited occurrence

--- src/services/recurrenceService.ts
+++ src/services/recurrenceService.ts
@@ -269,7 +269,10 @@ export const recurrenceService = {
     if (Object.prototype.hasOwnProperty.call(changes, 'sectionId')) current.sectionId = changes.sectionId ?? null
     if (Object.prototype.hasOwnProperty.call(changes, 'priority') && changes.priority !== undefined) current.priority = changes.priority
     if (Object.prototype.hasOwnProperty.call(changes, 'estimatedMinutes')) current.estimatedMinutes = changes.estimatedMinutes ?? null
-    if (Object.prototype.hasOwnProperty.call(changes, 'tags') && changes.tags !== undefined) current.tags = changes.tags
+    if (Object.prototype.hasOwnProperty.call(changes, 'tags') && changes.tags !== undefined) {
+      current.tags = changes.tags
+      current.tagIds = task.tagIds ?? []
+    }
     if (Object.prototype.hasOwnProperty.call(changes, 'tagIds') && changes.tagIds !== undefined) current.tagIds = changes.tagIds
     if (Object.prototype.hasOwnProperty.call(changes, 'checklist') && changes.checklist !== undefined) current.checklist = changes.checklist.map((item) => item.text)
     if (Object.prototype.hasOwnProperty.call(changes, 'sourceUrl')) current.sourceUrl = changes.sourceUrl ?? null
