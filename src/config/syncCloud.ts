0108a6f4ff7ff5adb949cb5f55e3ec8f9e310d71
thiepn <junsosmart@gmail.com>
2026-09-25 01:59:36 +0200

    D18: add public sync cloud configuration

---
+++ src/config/syncCloud.ts
@@ -0,0 +1,5 @@
+export const FOLIO_SYNC_CLOUD = {
+  projectRef: 'bskfihouwdogrunnglbg',
+  url: 'https://bskfihouwdogrunnglbg.supabase.co',
+  publishableKey: 'sb_publishable_wrBDATchmZXFFr3t8OXoQA_IsjlhrZM',
+} as const
